62ed484fea76a8c04c5cabb9897b34f8682a0f44
FehizoroFnprn <razfanamperana@gmail.com>
2023-03-01 20:12:43 +0400

    misy tsy mety kel item

--- src/Components/ItemFile.js
+++ src/Components/ItemFile.js
@@ -4,11 +4,11 @@ import imgPdfFile from '../assets/img/pdf-file.png';
 import imgDocFile from '../assets/img/doc-file.png';
 import imgTxtFile from '../assets/img/txt-file.png';
 
-const ItemFile = ({ filetype = 'pdf' }) => {
+const ItemFile = ({ filetype = 'pdf', filename = 'file.docx', fileSize = '10.25Mo', dateCreation = '0000-00-00 00:00:00' }) => {
     const dictFile = {
         'pdf': { class: 'pdf', src: imgPdfFile },
         'doc': { class: 'doc', src: imgDocFile },
-        'txt': { class: 'txt', src: imgTxtFile },
+        'txt': { class: 'txt', src: imgTxtFile }
     }
 
     return (
@@ -17,9 +17,9 @@ const ItemFile = ({ filetype = 'pdf' }) => {
                 <img src={dictFile[filetype].src} className="icon" alt="Documents icon" />
             </div>
             <div className="file__info">
-                <h3 className="name">file.docx</h3>
-                <span className="date">0000-00-00 00:00:00</span>
-                <span className="size">10.25Mo</span>
+                <h3 className="name">{filename}</h3>
+                <span className="date">{dateCreation}</span>
+                <span className="size">{fileSize}</span>
             </div>
         </div>
     )
